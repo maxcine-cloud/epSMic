5903b90d49e29b282360b7e0a88a0cc53a3f3ec8
maxcine-cloud <chaning2021@gmail.com>
2023-10-28 22:30:39 +0800

    Update rank_mrmr.py

--- src/rank_mrmr.py
+++ src/rank_mrmr.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def rankmrmr(ways,XtrainR,YtrainR):
     XtrainR.columns = XtrainR.columns.astype(str)
-    res=pymrmr.mRMR(XtrainR, 'MIQ', XtrainR.shape[0])
+    res=pymrmr.mRMR(XtrainR, 'MIQ', XtrainR.shape[1])
     X_train=XtrainR.loc[:,res]
     print(X_train.columns)
     # np.savetxt('./out/inte_data/'+ways+'rank_colunms.txt',np.array(X_train.columns.tolist()),delimiter='\t',fmt='%s')    
